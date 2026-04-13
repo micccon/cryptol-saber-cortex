4fbff13a4a199c75f68a28be764743edc9db4e11
Ryan Pettes <ryan.pettes@gmail.com>
2026-04-13 12:01:49 -0400

    Instantiate params.h

---
+++ C-Implementation/include/params.h
@@ -0,0 +1,10 @@
+/**
+ * This file to contain all constants used in Saber implementations
+ * (of varying security levels), per Section 8.1 (Implementation Constants)
+ * of the Round 3 Saber Specification.
+ */
+
+#ifndef PARAMS_H
+#define PARAMS_H
+
+#endif
