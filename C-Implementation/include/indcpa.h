74d64c3aea812abeeebe8c34d41a32514a0f1692
gottjig <gottjig@vt.edu>
2026-04-15 12:55:18 -0400

    indcpa.h stubs + type

--- C-Implementation/include/indcpa.h
+++ C-Implementation/include/indcpa.h
@@ -17,4 +17,41 @@
 #ifndef INDCPA_H
 #define INDCPA_H
 
+#include "params.h"
+#include <stdint.h>
+
+typedef struct {
+    uint8_t seed_a[SABER_SEEDBYTES];
+    uint8_t pk[SABER_PUBLICKEYBYTES - SABER_SEEDBYTES];
+} pk_t;
+
+/**
+ * Generates a random public/secret keypair (Algorithm 17)
+ *
+ * @param pk empty pk struct (byte string array of SABER_INDCPA_PUBKEYBYTES)
+ * @param sk empty byte string array of SABER_INDCPA_SECRETKEYBYTES
+ */
+void PKE_KeyGen(pk_t *pk, uint8_t *sk);
+
+/**
+ * Encrypt a message m with a public key (Algorithm 18)
+ *
+ * @param m message to encrypt - 256 bit string
+ * @param seed_s randome byte string of SABER_SEEDBYTES
+ * @param pk public key byte string to use to encrypt
+ *
+ * @return ciphertext byte string
+ */
+uint8_t *PKE_Enc(uint8_t *m, uint8_t *seed_s, pk_t *pk);
+
+/**
+ * Decrypt a ciphertext ct with a secret key sk
+ * 
+ * @param ct ciphertext to decrypt - byte string
+ * @param sk secret key to use to encrypt
+ *
+ * @return 256 bit string message
+ */
+uint8_t *PKE_DEC(uint8_t *ct, uint8_t *sk);
+
 #endif
